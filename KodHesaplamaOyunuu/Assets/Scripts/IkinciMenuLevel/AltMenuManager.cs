543724ace4367682dd9bba2a3c247d4f6032b907
Bilge-can <bilgecan618@gmail.com>
2023-06-08 15:54:04 +0300

    geriDonButonKodlama

--- KodHesaplamaOyunuu/Assets/Scripts/IkinciMenuLevel/AltMenuManager.cs
+++ KodHesaplamaOyunuu/Assets/Scripts/IkinciMenuLevel/AltMenuManager.cs
@@ -2,13 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class AltMenuManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject AltMenuPaneli;
     
     void Start()
     {
-        
+        AltMenuPaneli.GetComponent<CanvasGroup>().DOFade(1, 1f);
+        AltMenuPaneli.GetComponent<RectTransform>().DOScale(1, 1f).SetEase(Ease.OutBack);
     }
 
     public void oyunAcilsin(string oyunAcilsin)
@@ -16,5 +20,9 @@ public class AltMenuManager : MonoBehaviour
         PlayerPrefs.SetString("oyunAcilsin", oyunAcilsin);
         SceneManager.LoadScene("GameLevel");
     }
-    
+
+    public void GeriDon()
+    {
+        SceneManager.LoadScene("MenuLevel");
+    }
 }
