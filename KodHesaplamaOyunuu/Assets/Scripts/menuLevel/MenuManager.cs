5be7bf44c0772d2adf26f68cddbb662db8082dac
Bilge-can <bilgecan618@gmail.com>
2023-06-08 11:49:45 +0300

    MenuLevelKodlama

--- KodHesaplamaOyunuu/Assets/Scripts/menuLevel/MenuManager.cs
+++ KodHesaplamaOyunuu/Assets/Scripts/menuLevel/MenuManager.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private GameObject menuPaneli;
+    
     void Start()
     {
-        
+        menuPaneli.GetComponent<CanvasGroup>().DOFade(1, 1f);
+        menuPaneli.GetComponent<RectTransform>().DOScale(1, 1f).SetEase(Ease.OutBack);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ikinciLeveleGec()
     {
-        
+        SceneManager.LoadScene("ikinciMenuLevel");
     }
+
+    public void AyarlariYap()
+    {
+        //daha sonra kodlaması yapılacak
+    }
+
+    public void OyundanCik()
+    {
+        Application.Quit();
+    }
+
+    
 }
