f2ccfd32602845815fc17015d937aadb2f1e4e8a
Bilge-can <bilgecan618@gmail.com>
2023-06-11 21:36:16 +0300

    sonucPaneliKodlama

---
+++ KodHesaplamaOyunuu/Assets/Scripts/GameLevel/SonucManager.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class SonucManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private Image sonucImage;
+
+    [SerializeField]
+    private Text dogruText, yanlisText,puanText;
+
+    [SerializeField]
+    private GameObject tekrarOynaBtn,anaMenuBtn;
+
+    float sureTimer;
+    bool resimAcilsinmi;
+
+
+    
+    void Start()
+    {
+        sureTimer = 0;
+        resimAcilsinmi = true;
+
+        dogruText.text = "";
+        yanlisText.text = "";
+        puanText.text = "";
+
+        tekrarOynaBtn.GetComponent<RectTransform>().localScale = Vector3.zero;
+        anaMenuBtn.GetComponent<RectTransform>().localScale = Vector3.zero;
+
+        StartCoroutine(ResimAcRoutine());
+
+    }
+
+    
+    IEnumerator ResimAcRoutine()
+    {
+        while (resimAcilsinmi)
+        {
+            sureTimer += .15f;
+            sonucImage.fillAmount = sureTimer;
+
+            yield return new WaitForSeconds(0.1f);
+
+            if (sureTimer >= 1)
+            {
+                sureTimer = 1;
+                resimAcilsinmi = false;
+
+                dogruText.text = "10 DOĞRU";
+                yanlisText.text = "10 YANLIŞ";
+                puanText.text = "250 PUAN";
+
+                tekrarOynaBtn.GetComponent<RectTransform>().DOScale(1, .3f);
+                anaMenuBtn.GetComponent<RectTransform>().DOScale(1, .3f);
+
+            }
+        }
+    }
+}
